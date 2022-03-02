b2d8a9b5bb4dcb27bd510dd540bfedc05d10e1cb
Marc Lozano Martinez <36260994+Kendo777@users.noreply.github.com>
2022-03-03 00:30:12 +0100

    Add files via upload

---
+++ GameOfLife/MultiThreadTaskManager.cs
@@ -0,0 +1,289 @@
+﻿//#define USING_SEMAPHORE_SOLUTION
+//#define USING_DIVISION_SOLUTION
+#define USING_DINAMIC_SOLUTION
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace GameOfLife
+{
+    class MultiThreadTaskManager
+    {
+        public const int MaxNumberOfThreads = 8;
+        public static bool exit = false;
+        //Only for testing
+        private volatile static int numberOfThreads = 0;
+
+        private volatile static int doneThreads = 0;
+        //Matrix for read
+        private static bool[,] originalMatrix;
+        //Matrix for write
+        private static bool[,] copyMatrix;
+
+        private static int rows;
+        private static int columns;
+        private static object lockObj = new object();
+        private static Semaphore waitingThread = new Semaphore(MaxNumberOfThreads, MaxNumberOfThreads);
+
+#if USING_DIVISION_SOLUTION
+        private static int numOfCellsPerThread;
+#endif
+#if USING_DINAMIC_SOLUTION
+        private static Thread[] threads = new Thread[MaxNumberOfThreads];
+        private static int[] taskArrived = new int[MaxNumberOfThreads];
+#endif
+#if USING_SEMAPHORE_SOLUTION
+        public static void addTask(int index)
+        {
+        //Thanks to the semaphore there can only been a max number of threads at the same time
+            waitingThread.WaitOne();
+            {
+                Thread thread = new Thread(cellThread);
+                thread.IsBackground = true;
+                thread.Start(index);
+            }
+        }
+
+        public static void cellThread(object indexObj)
+        {
+
+            //Get the x an y 
+            int y = (int)indexObj % columns;
+            int x = ((int)indexObj -y)/columns;
+
+            //For debugging
+            int threadNumber = numberOfThreads;
+            ++numberOfThreads;
+            string threadName = string.Format("Thread {0}", threadNumber);
+            Thread.CurrentThread.Name = threadName;
+            //Console.WriteLine("Thread {0} - Cell [{1},{2}]", threadNumber, x, y);
+
+            //Cell functions
+            int neighbours = getNeighbours(x, y);
+            rulesOfLife(x, y, neighbours);
+
+            //Only one thread per time will increase the variable
+            lock(lockObj)
+            {
+                doneThreads++;
+            }
+            //Debugging
+            //Thread.Sleep(100);
+
+            //When a thread is finished we release the semaphore for let another one to start
+            waitingThread.Release();
+
+        }
+#endif
+#if USING_DIVISION_SOLUTION
+        public static void addTask()
+        {
+        //Only create the max number of threads allowed
+            for (int i = 0; i < MaxNumberOfThreads; i++)
+            {
+                Thread thread = new Thread(cellThread);
+                thread.IsBackground = true;
+                //Pass the starting cell of each thread
+                thread.Start(numOfCellsPerThread * i);
+            }
+        }
+        public static void cellThread(object indexObj)
+        {
+            //Get the x an y 
+            int y = (int)indexObj % columns;
+            int x = ((int)indexObj - y) / columns;
+
+            //For debugging
+            int threadNumber = numberOfThreads;
+            ++numberOfThreads;
+            string threadName = string.Format("Thread {0}", threadNumber);
+            Thread.CurrentThread.Name = threadName;
+
+            //Debugging
+            //Console.WriteLine("Thread {0} - Cell [{1},{2}]", threadNumber, x, y);
+
+            int cells;
+            //Check if the number of cells per thread is going to be the predefinide (rows*col)/numMaxThreads or if its going to be less for not get out of index
+            if((int)indexObj+numOfCellsPerThread >rows*columns)
+            {
+                cells = (int)indexObj - rows * columns - 1;
+            }
+            else
+            {
+                cells = numOfCellsPerThread;
+            }
+            //Cell functions
+            for (int i = 0; i < cells; i++)
+            {
+                y = (int)indexObj % columns;
+                x = ((int)indexObj - y) / columns;
+                int neighbours = getNeighbours(x, y);
+                rulesOfLife(x, y, neighbours);
+                indexObj = (int)indexObj + 1;
+            }
+            //Only one thread per time will increase the variable
+            lock (lockObj)
+            {
+                doneThreads++;
+            }
+            
+        }
+#endif
+#if USING_DINAMIC_SOLUTION
+        public static void addTask(object index)
+        {
+            //Each cell is trying to add a task to the manager, but it only happens if thers an available thread
+            waitingThread.WaitOne();
+            //Instead of using semaphores we can use a while loop until you can make the task, but we will use a semaphore for show the use of it
+            //while(!stop)
+            {
+                for (int i = 0; i < MaxNumberOfThreads; i++)
+                {
+                    //If we have a waiting thread we will assign it a task changing the cell index
+                   if (taskArrived[i]==-1)
+                    {
+                        taskArrived[i] = (int)index;
+                        break;
+                    }
+                }
+            }
+           
+        }
+        /*
+         * The thread will be allways executed, but unlease it has an valid cell it wont do nothing 
+         */
+        public static void cellThread()
+        {
+            while (!exit)
+            {
+                if (taskArrived[int.Parse(Thread.CurrentThread.Name)]!=-1)
+                {
+                    int y = (int)taskArrived[int.Parse(Thread.CurrentThread.Name)] % columns;
+                    int x = ((int)taskArrived[int.Parse(Thread.CurrentThread.Name)] - y) / columns;
+                    //Debugging
+                    //Console.WriteLine("{0} - Cell [{1},{2}]", Thread.CurrentThread.Name, x, y);
+
+                    //Cell functions
+                    int neighbours = getNeighbours(x, y);
+                    rulesOfLife(x, y, neighbours);
+
+                    //Only one thread per time will increase the variable
+                     lock (lockObj)
+                     {
+                         doneThreads++;
+                    }
+                     // change thread state to let him now that he done the work with this cell
+                    taskArrived[int.Parse(Thread.CurrentThread.Name)] = -1;
+                    waitingThread.Release();
+                    //Thread.Sleep(5000);
+                }
+                else
+                {
+                    Thread.Sleep(0);
+                }
+            }
+        }
+#endif
+        private static int getNeighbours(int row, int column)
+        {
+            int aliveNeighbors = 0;
+            // find your alive neighbors
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (row + i >= 0 && row + i < rows && column + j >= 0 && column + j < columns)
+                        if (originalMatrix[row + i, column + j])
+                        {
+                            aliveNeighbors++;
+                        }
+                }
+            }
+            // The cell needs to be subtracted from its neighbors as it was  counted before 
+            if (originalMatrix[row, column])
+            {
+                aliveNeighbors--;
+            }
+
+            return aliveNeighbors;
+        }
+        private static void rulesOfLife(int row, int column, int aliveNeighbors)
+        {
+            // Implementing the Rules of Life 
+            bool currentCell = originalMatrix[row, column];
+            // Cell is lonely and dies 
+            if (currentCell == true && aliveNeighbors < 2)
+            {
+                copyMatrix[row, column] = false;
+            }
+
+            // Cell dies due to over population 
+            else if (currentCell == true && aliveNeighbors > 3)
+            {
+                copyMatrix[row, column] = false;
+            }
+
+            // A new cell is born 
+            else if (currentCell == false && aliveNeighbors == 3)
+            {
+                copyMatrix[row, column] = true;
+            }
+            // stays the same
+            else
+            {
+                copyMatrix[row, column] = currentCell;
+            }
+        }
+
+        /*
+         * Initialize variables
+         */
+        public static void setVariables(int r, int c)
+        {
+            rows = r;
+            columns = c;
+#if USING_DIVISION_SOLUTION
+            //Calculate the number of cells that each thread must procese
+            numOfCellsPerThread = rows * columns / MaxNumberOfThreads;
+            if(numOfCellsPerThread % 1 !=0)
+            {
+                numOfCellsPerThread++;
+            }
+#endif
+#if USING_DINAMIC_SOLUTION
+            //Create the threads
+            for (int i = 0; i < MaxNumberOfThreads; i++)
+            {
+                taskArrived[i] = -1;
+                threads[i] = new Thread(cellThread);
+                threads[i].Name = i.ToString();
+                threads[i].IsBackground = true;
+                threads[i].Start();
+            }
+#endif
+        }
+        /*
+         Reset the variables of the new iteration and add the old matrix to modify 
+         */
+        public static void setMatrix(bool[,] matrix)
+        {
+            numberOfThreads = 0;
+            doneThreads = 0;
+            originalMatrix = matrix;
+            copyMatrix = new bool[rows,columns];
+        }
+        public static bool[,] getMatrix()
+        {
+            return copyMatrix;
+        }
+        public static int getNumOfFinishedThreads()
+        {
+            return doneThreads;
+        }
+        public static void exitTask()
+        {
+            exit = true;
+        }
+    }
+}
+
---
+++ GameOfLife/Program.cs
@@ -0,0 +1,215 @@
+﻿//#define USING_DIVISION_SOLUTION
+#define USING_DINAMIC_SOLUTION
+using System;
+using System.Threading;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        public static int Rows = 64;
+        public static int Columns = 64;
+
+        public static char aliveChar = 'O';
+        public static char deathChar = 'X';
+        private static bool quit = false;
+
+        private static string[] fileLines;
+        public static int stepTime = 100; // In my opinion 100 is very fast
+        static void Main(string[] args)
+        {
+
+            // randomly initialize our grid with the difined Rows and Columns, for doing that you must to coment the line of bool[,] lifeGameMatrix = readFile();
+
+            /*bool[,] lifeGameMatrix = new bool[Rows,Columns];
+            Random random = new Random();
+             for (var row = 0; row < Rows; row++)
+            {
+                for (var column = 0; column < Columns; column++)
+                {
+                    int num = (int)random.Next(0, 2);
+                    if (num!=0)
+                        lifeGameMatrix[row, column] = true;
+                    else
+                        lifeGameMatrix[row, column] = false;
+                }
+            }*/
+
+            //First of all read the file and create the matrix
+            bool[,] lifeGameMatrix = readFile();
+
+            //Set variables in MultiThreadTaskManager as rows and columns
+            MultiThreadTaskManager.setVariables(Rows, Columns);
+
+            //Print first state of the game
+            Print(lifeGameMatrix);
+
+            //Start the game by a Thread
+            Thread gameOfLifeThread = new Thread(iterateLifeGameThread);
+            gameOfLifeThread.Start(lifeGameMatrix);
+
+            //Stop the game if you press Q
+            while (!quit)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                quit = (keyInfo.Key == ConsoleKey.Q);
+                MultiThreadTaskManager.exitTask();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Finishing program");
+        }
+
+        private static bool[,] readFile()
+        {
+            //File propiedades.txt is inside the project, if you want to use a bigger matrix, change propiedades.txt to test.txt or use yours adding it to GameOfLife\GameOfLife\bin\Debug\netcoreapp3.1\Data
+            fileLines = System.IO.File.ReadAllLines(Environment.CurrentDirectory+@"\Data\propiedades.txt");
+            
+            //Separate the first line by , to obtain the rows and columns
+            string[] aux = fileLines[0].Split(',');
+            char[] charAux;
+            Rows = int.Parse(aux[0]);
+            Columns = int.Parse(aux[1]);
+            //initialize the matrix
+            bool[,] matrix = new bool[Rows, Columns];
+            int pointer = 1;
+
+            for (int row = 0; row < fileLines.Length - 1; row++)
+            {
+                //Get the lines of the text and divide it by individual chars
+                charAux = fileLines[pointer].ToCharArray();
+                //If the user didnt complete the line with the same number of chars as columns we will complete the diference with death cells
+                if (charAux.Length < Columns)
+                {
+                    
+                    for (int i = 0; i < charAux.Length; i++)
+                    {
+                        //Check alive cells or death cells of txt
+                        if (charAux[i] != ' ')
+                        {
+                            matrix[row, i] = true;
+                        }
+                        else
+                        {
+                            matrix[row, i] = false;
+                        }
+                    }
+                    //rest of columns
+                    for (int i = 0; i < Columns - charAux.Length; i++)
+                    {
+                        matrix[row, i + charAux.Length] = false;
+                    }
+                }
+                else
+                {
+                    for (int column = 0; column < Columns; column++)
+                    {
+                        if (charAux[column] != ' ')
+                        {
+                            matrix[row, column] = true;
+                        }
+                        else
+                        {
+                            matrix[row, column] = false;
+                        }
+                    }
+                }
+                pointer++;
+            }
+            //If the user didnt complete the line with the same number of chars as rows we will complete the diference with death cells
+            if (fileLines.Length - 1 < Rows)
+            {
+                for (int row = fileLines.Length - 1; row < Rows; row++)
+                {
+                    for (int col = 0; col < Columns; col++)
+                    {
+                        matrix[row, col] = false;
+                    }
+                }
+            }
+            return matrix;
+        }
+#if USING_DIVISION_SOLUTION
+        private static void iterateLifeGameThread(object lifeGameMatrix)
+        {
+            while (!quit)
+            {
+                //Pass the matrix to the MultiThreadTaskManager for make a copy
+                MultiThreadTaskManager.setMatrix((bool[,])lifeGameMatrix);
+
+                //Start the function of dividing the matrix
+                MultiThreadTaskManager.addTask();
+                while (!quit)
+                {
+                    if (MultiThreadTaskManager.getNumOfFinishedThreads() == MultiThreadTaskManager.MaxNumberOfThreads)
+                    {
+                        break;
+                    }
+                }
+                if (!quit)
+                {
+                //Change the matrix for the modify Matrix and print it
+                    lifeGameMatrix = MultiThreadTaskManager.getMatrix();
+                    Print((bool[,])lifeGameMatrix);
+                }
+                Thread.Sleep(stepTime);
+            }
+        }
+#else
+        private static void iterateLifeGameThread(object lifeGameMatrix)
+        {
+            while (!quit)
+            {
+                //Pass the matrix to the MultiThreadTaskManager for make a copy
+                MultiThreadTaskManager.setMatrix((bool[,])lifeGameMatrix);
+
+                //Each cell is sending a request to the MultiThreadTaskManager for making an action
+                for (int row = 0; row < Rows; row++)
+                {
+                    for (int column = 0; column < Columns; column++)
+                    {
+                        MultiThreadTaskManager.addTask(row * Columns + column);
+                    }
+                }
+                while (!quit)
+                {
+                    if (MultiThreadTaskManager.getNumOfFinishedThreads() == Rows * Columns)
+                    {
+                        break;
+                    }
+                }
+                if (!quit)
+                {
+                    //Change the matrix for the modify Matrix and print it
+                    lifeGameMatrix = MultiThreadTaskManager.getMatrix();
+                    Print((bool[,])lifeGameMatrix);
+                }
+                Thread.Sleep(stepTime);
+            }
+        }
+#endif
+        private static void Print(bool[,] matrix)
+        {
+            Console.Clear();
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int column = 0; column < Columns; column++)
+                {
+                    bool cell = matrix[row, column];
+                    if (cell == true)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(aliveChar);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(deathChar);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
